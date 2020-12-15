19cfd1a554375c8dbf1d9dabb209f45bd4905830
kemiranda <kjmd2s@gmail.com>
2020-12-15 09:20:35 -0500

    Segundo commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <v-app>
+        <v-content transition="slide-x-transition">
+            <router-view></router-view>
+        </v-content>
+    </v-app>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+//import HelloWorld from './components/HelloWorld'
+import Inicio from './components/Inicio'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Inicio
   }
 }
 </script>
 
 <style>
-#app {
+/*#app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+}*/
 </style>
---
+++ src/components/Inicio.vue
@@ -0,0 +1,142 @@
+<template>
+    <div id="User">
+        <h1> {{ msg }}}</h1>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap" rel="stylesheet">
+        
+        <!-- Contenedor de ventana -->
+        <div class="grid_container">
+        <!-- Componente de busqueda de usuario -->
+        <section class="search_container">
+            <legend>Buscar usuario</legend>
+            <div class="search_user_entry">
+                <input type="text" placeholder="Buscar por nombre">
+                <button v-on:click="search()" class="btn-primary">Buscar</button>
+            </div>
+        </section>
+        
+
+        <!-- Formulario de creación de usuario -->
+        <section class="form_container">
+            <div class="form_container_entry">
+                <form>
+                    <legend>Crear usuario</legend>
+                    <div>
+                        <input type="text" name="first_name" placeholder="Nombres" v-model="username" required>
+                    </div>
+                    <div>
+                        <input type="text" name="last_name" placeholder="Apellidos" v-model="surname" required>
+                    </div>
+                    <div>
+                        <input type="number" name="id_document" placeholder="Documento de identidad" v-model="id_document" required>
+                    </div>
+                    <div>
+                        <input type="email" name="email" placeholder="Correo electrónico" v-model="email" required>
+                    </div>
+                    <div>
+                        <input type="tel" name="telephone" placeholder="Número de teléfono" v-model="tel_number" required>
+                    </div>
+                    <div class="register_button">
+                        <hr/>
+                        <button v-on:click="authenticate()" class="btn-secondary">Registrar</button>
+                        <hr/>
+                        <p>¿Ya tiene una cuenta? - Buscar usuario</p> 
+                    </div>
+                </form>
+            </div>
+        </section>
+        
+        </div>
+    </div>
+</template>
+
+
+
+<script>
+    export default {
+        name: "Inicio",
+        data () {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        },
+
+        /*created: function(){
+            this.username = this.$route.params.username
+        }*/
+    }
+</script>
+
+
+<style>
+    template{
+    font-family: 'Quicksand', sans-serif;
+}
+
+.grid_container {
+    height: 100vh;
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(8, 1fr);
+    grid-template-rows: repeat(7, 1fr);
+    grid-column-gap: 2px;
+    grid-row-gap: 2px;
+    }
+    
+    .search_container {
+        
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        align-content: center;
+        grid-area: 1 / 1 / 8 / 5; 
+    }
+    .search_user_entry { grid-area: 4 / 2 / 5 / 4; 
+        width: 60%;
+        padding: 10px 50px;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+
+    }
+
+    .search_user_entry input{
+        width: 300px;
+        padding: 5px;
+    }
+
+    .form_container { 
+        
+        grid-area: 1 / 5 / 8 / 9;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        align-content: center; 
+    }
+    
+    .form_container_entry { 
+        grid-area: 2 / 6 / 7 / 8; 
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        align-content: center; 
+    }
+
+    .form_container_entry form{
+        text-align: center;
+    }
+
+    .form_container_entry form input{
+        width: 300px;
+        padding: 5px;
+        margin: 5px;
+    }
+
+    .btn-secondary{
+        width: 300px;
+        padding: 5px;
+    }
+
+</style>
