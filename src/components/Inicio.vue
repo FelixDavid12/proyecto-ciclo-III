9bf11b2643fec5c1a23e4f4c9e8db545cc53a864
LuisFCT <lufcastrotu@unal.edu.co>
2020-12-15 00:28:20 -0500

    Template del formulario para crear usuario

---
+++ src/components/Inicio.vue
@@ -0,0 +1,17 @@
+//Template del formulario para crear usuario
+<template>     
+    <div id="CreateUser">
+        <form id="form1" onsubmit="register()">
+            
+            <h2>Formulario de registro</h2><br>
+            Nombre: <input type="text" name="name_user"> 
+            &nbsp; Apellido: <input type="text" name="lastname_user"><br>
+            <br>Número de Identificación: <input type="text" name="cc_user"><br>
+            <br>Email: <input type="email" name="email_user"><br>
+            <br>Teléfono/Celular : <input type="text" name="tel_user"><br>
+
+            <br><button type="submit" >Registrar</button>
+            
+        </form>
+    </div>
+</template>
