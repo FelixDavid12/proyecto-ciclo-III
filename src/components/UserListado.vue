19615fe9cd89414ae2d9f685ea0979ea8a0fb2e8
Luis Fernando Castro Tulcan <75462964+LuisFCT@users.noreply.github.com>
2020-12-20 20:19:06 -0500

    Cambios CSS

--- src/components/UserListado.vue
+++ src/components/UserListado.vue
@@ -5,14 +5,19 @@
     <!-- Load icon library -->
 
       <div class="search_container"></div>
-          <div class="search_container_logo">
-            <h2>User Hotel<br>
-            <span>Conoce nuestros usuarios</span></h2>
+          
+          <div class="search_container_logo1">
+            <h2>User Hotel</h2>
+          </div>
+          
+          <div class="search_container_logo2">
+            <h2>Conoce nuestros Usuarios</h2>
+            <img src="@/assets/usuario.png" width="65px" height="65px" alt="Imagen1">
           </div>
       
-      <div class="search_container_btn">
-        <button v-on:click="getUsuarios">Listar Usuarios</button>
-      </div>
+      <!--<div class="search_container_btn">-->
+       <!-- <button v-on:click="getUsuarios">Listar Usuarios</button> -->
+      <!-- </div>-->
       
       <div class="result_container"></div>
         <div class="resul_container_json">
@@ -69,93 +74,76 @@ export default {
 </script>
 
 
-
-
 <style scoped>
-.UserListado {
+.UserListado{
+  background:white;
   display: grid;
-  grid-template-columns: repeat(8, 1fr);
-  grid-template-rows: repeat(5, 1fr);
-  grid-column-gap: 5px;
-  grid-row-gap: 5px;
+  grid-template-columns: 1fr 6fr 1fr;
+  grid-template-rows: 1fr 0.5fr 0.5fr 4fr 1fr;
 }
-.search_container {
-  grid-area: 1 / 1 / 6 / 5;
-  height: 85vh;
-  width: 100%;
+
+.search_container{
+  grid-area: 1 / 1 / 2 / 4;
+  background-color: white;
 }
-.search_container_logo {
-    grid-area: 3 / 2 / 4 / 4;
-    background-color: #283747;
-    border-radius: 10px;
-    display: flex;
-    justify-content: center;
-    flex-direction: row;
-    align-items: center;
-    align-content: center;
-    font-size: 120%;
-}
-.search_container_logo h2{
-    color: white;
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    align-items: center;
-    align-content: center;
-}
-.search_container_logo span{
-    color: red;
-    text-align: center;
-}
-.search_container_btn {
-  grid-area: 4 / 2 / 5 / 4;
+.search_container_logo1{
+  grid-area: 2 / 1 / 3 / 4;
+  background-color:white;
   display: flex;
   justify-content: center;
-  align-content: center;
-  max-height: 80px;
-  max-width: auto;
-  
 }
-.search_container_btn button{
-    width: 60%;
-    padding: 0px;
-    margin-top: 25px;
-    background: #2196F3;
-    border-radius: 50px;
-    color: white;
-    font-size: 18px;
-    border: 1px solid gray;
-    border-left: none; /* Prevent double borders*/
-    cursor: pointer;
-    font: "Verdana";
-    font-weight : bold;
-}
-.search_container_btn button:hover{
-      background-color: #9eb0e0;
-}
-.result_container {
-  grid-area: 1 / 5 / 6 / 9;
-  height: 90vh;
-  width: 100%;
-  display: flex;
-    
-}
-.resul_container_json {
-  grid-area: 2 / 5 / 5 / 8;
-  background-color: #ececec;
+
+.search_container_logo2{
+  grid-area: 3 / 2 / 4 / 3;
+  background-color: #3c99d7;
   border: 2px solid rgb(10, 10, 10);
-  border-radius: 10px;
+  border-radius: 5px;
   display: flex;
   justify-content: center;
-  flex-direction: column;
+}
+
+.search_container_logo1 h2{
+  display: flex;
+  justify-content: center;
+  color: #283747;
+  font-size: 30pt;
+}
+
+.search_container_logo2 h2{
+  color:black;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  font-size: 15pt;
+}
+
+.search_container_logo2 img{
+  padding: 0px;
+  margin-left: 540px;
+}
+
+.resul_container_json{
+  overflow-y: scroll;  
+  grid-area: 4 / 2 / 5 / 3;
+  background-color: #283747;
+  display: flex;
+  justify-content: center;
+  border: 2px solid rgb(10, 10, 10);
+  border-radius: 0px;
+  display: flex;
   align-items: center;
-  font-size: 120%;
-  font: "Verdana";
+  font-size: 100%;
+  color: white;
+ 
 }
+
 .resul_container_json p{
-    color: black;
-    font: "Verdana", bold;
+  color: white;
 }
 
+.result_container{
+  grid-area: 5 / 1 / 6 / 4;
+  background-color: white;
 
+}
 </style>
+
+
